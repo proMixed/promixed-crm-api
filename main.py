4ad88fbfc2844afa89a68223fbb269946a1b5286
proMixed <44735833+proMixed@users.noreply.github.com>
2025-06-30 13:22:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi.middleware.cors import CORSMiddleware
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from pydantic import BaseModel
 from typing import Optional
 import psycopg2
